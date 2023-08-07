ff7ddb7d30b38d1936e24bc2a6816d9b7448e4e2
Shubham Panchal <panchalshubham826@gmail.com>
2023-08-07 13:52:11 +0530

    Cannot find utils checking for prod bug fix

--- app/main.py
+++ app/main.py
@@ -12,13 +12,31 @@ from langchain.llms import OpenAI
 from langchain.chains import ConversationalRetrievalChain
 from flask_cors import CORS
 from langchain.chains.conversational_retrieval.prompts import CONDENSE_QUESTION_PROMPT
-from utils import PROMPT,ChainStreamHandler,ThreadedGenerator
+from langchain.prompts import PromptTemplate
+import queue
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+
 
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
 PINECONE_ENV = os.getenv('PINECONE_ENV')
 
 index_name = 'shubhampanchal'
+
+# Prompt stuff
+prompt_template = """You are an AI Avatar of Shubham Panchal. So answer every question in the first person narrative as shubham panchal. Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+
+{context}
+
+Question: {question}
+Answer the question as Shubham Panchal in the first person narrative"""
+
+PROMPT = PromptTemplate(
+    template=prompt_template, input_variables=["context", "question"]
+)
+
+
+
 
 app = Flask(__name__)
 CORS(app=app, resources={r'/*': {'origins': '*'}})
@@ -53,6 +71,39 @@ CORS(app=app, resources={r'/*': {'origins': '*'}})
 #     </html>
 #     ''', mimetype='text/html')
 
+
+
+# Thread Generator Class
+
+class ThreadedGenerator:
+    def __init__(self):
+        self.queue = queue.Queue()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        # removes the element from the queue and returns it.
+        item = self.queue.get()
+        if item is StopIteration:
+            raise item
+        return item
+
+    def send(self, data):
+        self.queue.put(data)
+
+    def close(self):
+        self.queue.put(StopIteration)
+
+# ChainStreamHandler class
+
+class ChainStreamHandler(StreamingStdOutCallbackHandler):
+    def __init__(self, gen):
+        super().__init__()
+        self.gen = gen
+
+    def on_llm_new_token(self, token: str, **kwargs):
+        self.gen.send(token)
 
 
 
